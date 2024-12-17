18adfb0dd097a466c16a023bfb50467151c17f9b
Starinin Andrey <starinin-andrey@ya.ru>
2024-12-17 21:15:11 +0300

    Initial commit

---
+++ Calculator.Core/Calc.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculator.Core;
+
+public static class Calc
+{
+    public static async Task<long> FactorialAsync(int number, CancellationToken cancellationToken = default,
+        IProgress<int>? progress = null)
+    {
+        long result = 1;
+        for (int i = number; i >= 1;  i--)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            progress?.Report(number - i);
+            
+            await Task.Delay(100, cancellationToken);
+            
+            result *= i;
+        }
+        
+        return result;
+    }
+
+    public static async Task<long> SumAsync(int startNum, int endNumber, long startSum, CancellationToken cancellationToken = default,
+        IProgress<(int, long)>? progress = null)
+    {
+        long result = startSum;
+        
+        for (int i = startNum; i <= endNumber; i++)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            result += i;
+            
+            progress?.Report((i, result));
+            
+            await Task.Delay(100, cancellationToken);
+        }
+
+        return result;
+    }
+}
---
+++ Calculator.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System.Reactive;
+using Calculator.Core;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Calculator.Desktop.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
+{
+    private CancellationTokenSource _ctsCalculateSum;
+    private CancellationTokenSource _ctsCalculateFactorial;
+
+    private int? _numberStartNum = null;
+    private long? _startSum = null;
+    private int? _numberFactorial;
+    
+    [Reactive] public string? InputNumberText { get; set; }
+    [Reactive] public string? OutputSumText { get; set; }
+    [Reactive] public string? OutputFactorialText { get; set; }
+    
+    [Reactive] public double ProgressSumValue { get; set; }
+    [Reactive] public double ProgressFactorialValue { get; set; }
+    
+    [Reactive] public string? StatusCalculateSum { get; set; }
+    [Reactive] public string? StatusCalculateFactorial { get; set; }
+    
+    public ReactiveCommand<Unit, Unit> CommandCalculateSumStart { get; }
+    public ReactiveCommand<Unit, Unit> CommandCalculateSumStop { get; }
+    
+    public ReactiveCommand<Unit, Unit> CommandCalculateFactorialStart { get; }
+    public ReactiveCommand<Unit, Unit> CommandCalculateFactorialStop { get; }
+
+    public MainWindowViewModel()
+    {
+        IObservable<bool> canExecuteCalculateSumStart = this.WhenAnyValue(
+            p1 => p1.InputNumberText,
+            p1 => !string.IsNullOrWhiteSpace(p1));
+
+        var canExecuteCalculateSumStop = this.WhenAnyValue(
+            p => p.ProgressSumValue,
+            p => p > 0);
+
+        CommandCalculateSumStart = ReactiveCommand.CreateFromTask(
+            execute: async () =>
+            {
+                _ctsCalculateSum = new CancellationTokenSource();
+
+                var startNum = _numberStartNum ?? 0;
+                var endNum = int.Parse(InputNumberText!);
+                var startSum = _startSum ?? 0;
+
+                var progress = new Progress<(int, long)>();
+                progress.ProgressChanged += (_, report) =>
+                {
+                    ProgressSumValue = report.Item1;
+                    
+                    _startSum = report.Item2;
+                    _numberStartNum = report.Item1 + 1;
+                };
+
+                try
+                {
+                    StatusCalculateSum = "Вычисление суммы...";
+                    
+                    var result = await Calc.SumAsync(
+                        startNum: startNum,
+                        endNumber: endNum,
+                        startSum: startSum,
+                        cancellationToken: _ctsCalculateSum.Token,
+                        progress: progress);
+                    
+                    OutputSumText = result.ToString();
+                    
+                    _numberStartNum = null;
+                    _startSum = null;
+                    
+                    StatusCalculateSum = "Вычисление суммы завершено";
+                }
+                catch (Exception)
+                {
+                    StatusCalculateSum = "Приостановка вычисления суммы";
+                }
+            },
+            canExecute: canExecuteCalculateSumStart);
+        
+        CommandCalculateSumStop = ReactiveCommand.CreateFromTask(
+            execute: async () =>
+            {
+                await _ctsCalculateSum!.CancelAsync();
+            },
+            canExecute: canExecuteCalculateSumStop);
+    }
+}
